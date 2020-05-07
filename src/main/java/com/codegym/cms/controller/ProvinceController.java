c6b7cee4d7d9759749ed369bca8ecbc90aa9e789
loiminh <loiho11082015@gmail.com>
2020-05-08 05:39:58 +0700

    add province

--- src/main/java/com/codegym/cms/controller/ProvinceController.java
+++ src/main/java/com/codegym/cms/controller/ProvinceController.java
@@ -1,16 +1,17 @@
 package com.codegym.cms.controller;
 
+import com.codegym.cms.model.Customer;
 import com.codegym.cms.model.Province;
 import com.codegym.cms.service.CustomerService;
 import com.codegym.cms.service.ProvinceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/province")
@@ -43,4 +44,72 @@ public class ProvinceController {
         redirect.addFlashAttribute("message", "create province successfully !");
         return "redirect:/province";
     }
+
+    @GetMapping("/edit/{id}")
+    public ModelAndView showFormEditProvince(@PathVariable long id){
+        Optional<Province> province = provinceService.findById(id);
+        if (province.isPresent()){
+            ModelAndView modelAndView = new ModelAndView("/province/edit");
+            modelAndView.addObject("province", province.get());
+            return modelAndView;
+        }else {
+            return new ModelAndView("/province/error");
+        }
+    }
+
+    @PostMapping("/edit")
+    public String updateProvince(@ModelAttribute("province") Province province, RedirectAttributes redirect){
+        provinceService.save(province);
+        redirect.addFlashAttribute("message","edit province successfully !");
+        return "redirect:/province";
+    }
+
+    @GetMapping("/delete/{id}")
+    public ModelAndView showFormDeleteProvince(@PathVariable Long id){
+        Optional<Province> province = provinceService.findById(id);
+        if (province.isPresent()){
+            ModelAndView modelAndView = new ModelAndView("/province/delete");
+            modelAndView.addObject("province", province.get());
+            return modelAndView;
+        }
+        else {
+            return new ModelAndView("province/error");
+        }
+    }
+
+    @PostMapping("/delete")
+    public String deleteProvince(@ModelAttribute("province") Province province, RedirectAttributes redirect){
+        provinceService.delete(province.getId());
+        redirect.addFlashAttribute("message", "delete province successfully !");
+        return "redirect:/province";
+    }
+
+    @GetMapping("/view/{id}")
+    public ModelAndView viewProvince(@PathVariable Long id){
+        Optional<Province> province = provinceService.findById(id);
+        if (province.isPresent()){
+            ModelAndView modelAndView = new ModelAndView("province/view");
+            modelAndView.addObject("province", province.get());
+            return modelAndView;
+        }
+        else {
+            return new ModelAndView("/province/error");
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
